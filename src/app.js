f1f24d47a73971f44fbe021acdd6c3bfdb85fae8
Daniel Cerverizzo <daniel.cerverizzo@cobrefacil.com.br>
2023-11-05 14:45:38 -0300

    fix: adjustment scraper class init function calling in app

--- src/app.js
+++ src/app.js
@@ -1,13 +1,10 @@
 const app = require('express')();
 const cors = require('cors');
 const jobRoutes = require('./routes/jobRoutes');
-const scraper = require('./cron/scraper')
+const Scraper = require('./cron/scraper')
 const cron = require('node-cron');
 
-// cron.schedule('25 12 * * *', () => {
-// });
-
-scraper.init();
+new Scraper().init();
 app.use(cors());
 app.use('/', jobRoutes(app));
 
