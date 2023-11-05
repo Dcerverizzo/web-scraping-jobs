2a01d6a58d01e6e98e41a0ee32c928b5e2a12b88
Daniel Cerverizzo <daniel.cerverizzo@cobrefacil.com.br>
2023-11-05 14:33:04 -0300

    fix: scraper not defined

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const cors = require('cors');
 const jobRoutes = require('./routes/jobRoutes');
-const Scraper = require('./cron/scraper')
+const scraper = require('./cron/scraper')
 const cron = require('node-cron');
 
 // cron.schedule('25 12 * * *', () => {
