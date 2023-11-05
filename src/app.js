883fe6e3acf47943a75db04d2f08c71cdb691bcb
Daniel Cerverizzo <daniel.cerverizzo@cobrefacil.com.br>
2023-11-05 14:13:28 -0300

    fix: removing cron job in code add in server

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const jobRoutes = require('./routes/jobRoutes');
 const Scraper = require('./cron/scraper')
 const cron = require('node-cron');
 
-cron.schedule('25 12 * * *', () => {
- scraper.init();
-});
+// cron.schedule('25 12 * * *', () => {
+// });
 
+scraper.init();
 app.use(cors());
 app.use('/', jobRoutes(app));
 
